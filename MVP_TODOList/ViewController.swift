26f85c0e1d6bbd9e6b70f831ee484318df876d8c
Kirill Drozdov <kirilldrozd12@gmail.com>
2022-06-19 17:22:29 +0300

    Initial Commit

---
+++ MVP_TODOList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVP_TODOList
+//
+//  Created by Kirill Drozdov on 19.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
